3e51ce6f346a32665f0a4da7057b0f3d729ae6e3
Armando <prieto.arm@gmail.com>
2019-05-27 02:08:17 -0700

    simple detail activity overview added

--- app/src/main/java/com/udacity/popularMovies/MovieDetailActivity.java
+++ app/src/main/java/com/udacity/popularMovies/MovieDetailActivity.java
@@ -9,14 +9,20 @@ import com.udacity.popularMovies.model.Movie;
 
 public class MovieDetailActivity extends AppCompatActivity {
     private TextView mMovieTitle;
+    private TextView mMovieOverview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
         mMovieTitle = (TextView) findViewById(R.id.tv_movie_title);
+        mMovieOverview = (TextView) findViewById(R.id.tv_movie_overview);
         Intent currentIntent = getIntent();
         if(currentIntent.hasExtra(Movie.TITLE))
-        mMovieTitle.setText(currentIntent.getStringExtra(Movie.TITLE));
+            mMovieTitle.setText(currentIntent.getStringExtra(Movie.TITLE));
+        if(currentIntent.hasExtra(Movie.OVERVIEW))
+            mMovieOverview.setText(currentIntent.getStringExtra(Movie.OVERVIEW));
+
 
     }
 }
